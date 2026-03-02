d4c1a4c185581b95e7a58afc7fa3ef7a9214c054
Israt-Jahan-Sujana <isratsujama25@gmail.com>
2026-03-02 07:18:29 +0600

    newproject

---
+++ translator.py
@@ -0,0 +1,21 @@
+"""
+Giraffe Language
+vowels- s
+
+example:
+dog -> dsg
+cat -> cst
+"""
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation=translation+"S"
+            else:
+                translation=translation+"s"
+        else:
+            translation= translation+letter
+    return translation
+print(translate(input("Enter a phrase: ")))
